c569888bce8739e3eb8f510776b8d3efe42508b9
yuewithme <126285112+yuewithme@users.noreply.github.com>
2026-07-17 11:28:49 +0800

    feat: align roadshow with sales workbench

--- admin-web/src/views/demo-chat/index.vue
+++ admin-web/src/views/demo-chat/index.vue
@@ -35,9 +35,6 @@
             <p>{{ item.content }}</p>
           </div>
         </div>
-        <div v-if="loading" class="message-row agent">
-          <div class="bubble thinking">销售 Agent 正在思考…</div>
-        </div>
       </div>
 
       <aside v-if="latestResult" class="agent-state">
@@ -250,10 +247,6 @@ const sendMessage = async () => {
   font-size: 12px;
   font-weight: 700;
   opacity: 0.72;
-}
-
-.thinking {
-  color: #60786e;
 }
 
 .agent-state {
